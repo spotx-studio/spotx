b9a9327a871559bb5db4d651c6ba131ea94f1f24
Gabriel Chiu <gabriel@spotx.studio>
2025-08-26 01:14:52 +0800

    case page

--- js/offcanvas-custom.js
+++ js/offcanvas-custom.js
@@ -6,9 +6,28 @@ $(document).ready(function () {
         $("body").toggleClass("unscrollable")
     });
 
+    $(".case-box").click(function (e) { 
+        e.preventDefault();
+        $(".case-preview").toggleClass("show");
+        $(".overlay").toggleClass("show");
+        $("body").toggleClass("unscrollable")
+    });
+
+    $(".uneo").click(function (e) { 
+        e.preventDefault();
+        $(".project-name").text("Uneo | Pressure Map APP");    
+        $(".desc-list li").each(function() {
+        let hasText = $.trim($(this).text()) !== "";
+        let hasImgOnly = $(this).children().length > 0 && !hasText;
+        if (!hasText && hasImgOnly) {
+            $(this).addClass("d-none");
+        }});
+    });
+
     $(".overlay").click(function (e) { 
         e.preventDefault();
         $(".side-menu").removeClass("show");
+        $(".case-preview").removeClass("show");
         $(".overlay").removeClass("show");
         $("body").removeClass("unscrollable");
     });
